6658e096e1b02c42dccd857e1569adfb363d42fa
zoomrcd12fa <63201132+zoomrcd12fa@users.noreply.github.com>
2020-08-27 22:24:40 +0200

    eliminate a warning

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { YellowBox } from 'react-native';
 
 import RootNavigator from './src/navigation/RootNavigator';
+
+YellowBox.ignoreWarnings([
+  'Non-serializable values were found in the navigation state',
+]);
 
 class App extends React.Component {
   render(){
